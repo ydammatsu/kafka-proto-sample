b0ce4c8f6a29502221745da33c7516eb866eb6f3
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-09-11 08:49:45 +0900

    コメント追加

--- bin/consumer.rb
+++ bin/consumer.rb
@@ -4,6 +4,7 @@ require 'date'
 require 'ruby-kafka'
 require './pb/sample_pb'
 
+# docker-compose で立ち上げるときは kafka:9092、ローカルで立ち上げるときは localhost:29092 とする
 kafka = Kafka.new(['kafka:9092'], client_id: 'consumer1')
 
 trap('TERM') { consumer.stop }
@@ -11,14 +12,11 @@ trap('TERM') { consumer.stop }
 consumer = kafka.consumer(group_id: 'group1')
 
 begin
+  # docker-compose.yml で depends_on を指定していても kafka の起動直後、 subscribe が失敗するときがありリトライしている
   consumer.subscribe('topic1')
 rescue Kafka::ConnectionError
   sleep 1
   retry
-end
-
-def serealize_message(message)
-  message.value
 end
 
 consumer.each_message(automatically_mark_as_processed: true) do |message|
--- bin/producer.rb
+++ bin/producer.rb
@@ -7,7 +7,7 @@ require './pb/sample_pb'
 kafka = Kafka.new(['kafka:9092'], client_id: 'producer1')
 topic = 'topic1'
 
-# topic の作成
+# topic が未作成の時、consumer で subscribe するときにエラーが発生するので、topic を作成しておく。
 def create_topic(kafka, topic)
   kafka.create_topic(topic)
   puts 'topic を作成しました'
@@ -18,21 +18,13 @@ rescue Kafka::ConnectionError
   retry
 end
 
-# topic にメッセージを送る
-def send_message(kafka, topic)
-  producer = kafka.producer
-
-  # こんな感じのバイナリになる => "\n\x04\x12\v\b\xAD\x82\xF1\x98\x06\x10\xF0\xC0\x83z"
-  message = Sample::TestMessage.new(content: ARGV.first, created_at: Time.now).to_proto
-
-  producer.produce(message, topic: topic)
-  producer.deliver_messages
-  puts 'メッセージを送信しました'
-end
-
 # 引数を渡さなければ topic を作成し、引数を渡せばその値でメッセージを送信する。
 if ARGV.empty?
   create_topic(kafka, topic)
 else
-  send_message(kafka, topic)
+  producer = kafka.producer
+  message = Sample::TestMessage.new(content: ARGV.first, created_at: Time.now).to_proto # こんな感じのバイナリになる => "\n\x04\x12\v\b\xAD\x82\xF1\x98\x06\x10\xF0\xC0\x83z"
+  producer.produce(message, topic: topic)
+  producer.deliver_messages
+  puts 'メッセージを送信しました'
 end
